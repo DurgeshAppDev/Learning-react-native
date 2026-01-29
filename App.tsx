04dad80784a77fa117efae3dfe45aeb1adc88219
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-01-29 21:08:35 +0530

    learned flex  and touchableHiglights buttons

--- App.tsx
+++ App.tsx
@@ -1,25 +1,83 @@
-// import { useEffect, useState } from 'react';
 import { useState } from 'react';
-import { View, Text,Button} from 'react-native';
-import Comp from "./Component/Comp.js";
+import { View, Text,StyleSheet, TouchableHighlight} from 'react-native';
+import {Comp,Greet,Search,Submit} from './Component/Comp.js' ;
 
+const App=()=>{
+ let [press,setpress]=useState(null);
 
-const App=() =>{
-  const [show,setShow]=useState(false);
   return(
-    <View>
-      <Text style={{fontSize:20,color:"orange",textAlign:"center",marginTop:10}}>
-        show and hide component
-        </Text>
-      <Button title='toggle' onPress={()=> setShow(!show)} />
-        {
-          show==true
-          ? <Comp />
-          :null
-        }
+    <View style={style.main}>
+      <View style={style.halfBox}>
+     <View style={style.singleComponent}>
+      <TouchableHighlight onPress={()=>setpress("hello")}>
+       <Text style={style.textStyle}>hello</Text>
+       </TouchableHighlight>
+       
+     </View>
+     <View style={style.singleComponent}>
+      <TouchableHighlight onPress={()=>setpress("Greet")}>
+       <Text style={[style.textStyle,style.Greet]}>Greet</Text>
+       </TouchableHighlight>
+     </View>
+     <View style={style.singleComponent}>
+      <TouchableHighlight onPress={()=>setpress("search")}>
+       <Text style={[style.textStyle,style.search]}>search</Text>
+       </TouchableHighlight>
+     </View>
+    <View style={style.singleComponent}>
+      <TouchableHighlight onPress={()=>setpress("submit")}>
+       <Text style={[style.textStyle,style.submit]}>submit</Text>
+       </TouchableHighlight>
+     </View>
+     </View>
+     {press === 'hello' && <Comp/>}
+     {press === 'Greet' && <Greet/>}
+     {press === 'search' && <Search/>}
+     {press === 'submit' && <Submit/>}
+
     </View>
   )
 }
 
-
+const style=StyleSheet.create({
+  main:{
+    flex:1,
+    margin:10,
+  },
+  halfBox:{
+    width:'100%',
+    height:'50%',
+    flexDirection:'row',
+    flexWrap:'wrap'
+  },
+  singleComponent:{
+  width:'50%',
+  height:'50%',
+  justifyContent:'center',
+  alignItems:'center',
+  borderWidth:1,
+  borderRadius:10,
+  borderColor:'#120202'
+  },
+  textStyle:{
+    fontSize:20,
+    backgroundColor:'#f30000',
+    color:'#000000',
+    textAlign:'center',
+    padding:5,
+    margin:10,
+    borderRadius:5,
+    shadowColor:'black',
+    elevation:10
+    },
+  Greet:{
+    backgroundColor:'green'
+  },
+  search:{
+    backgroundColor:'blue'
+  },
+  submit:{
+    backgroundColor:'yellow'
+  }
+})
 export default App;
--- Component/Comp.js
+++ Component/Comp.js
@@ -2,29 +2,42 @@ import React, { useEffect } from "react";
 import { View, Text , StyleSheet} from "react-native";
 
 
-const Comp=()=> {
- let time= setInterval(()=>{
-    console.log("hii");
-  },3000)
-  useEffect(()=>{
-   return ()=> clearInterval(time)
-  })
+export const Comp=()=> {
     return (
       <View>
         <Text style={style.styling}>MY NAME IS ALICE</Text>
         <Text style={style.styling}>LEARING REACTNATIVE</Text>
         <Text style={style.styling}>USING TOGGLE</Text>
-        
-      </View>
+       </View>
     );
+}
+
+export const Greet=()=>{
+  return(
+    <View>
+      <Text style={style.styling}>welcome to my Application</Text>
+    </View>
+  )
+}
+export const Search=()=>{
+  return(
+    <View>
+      <Text style={style.styling}>Searching for anything thats:_____</Text>
+    </View>
+  )
+}
+export const Submit=()=>{
+  return(
+    <View>
+      <Text style={style.styling}>submitted data succesfully</Text>
+    </View>
+  )
 }
 const style=StyleSheet.create({
   styling:{
-    fontSize:20,
-    color:"orange",
-    textAlign:"center",
-    marginTop:10
+    textAlign:'center',
+    color:'black',
+    fontSize:20
   }
 })
 
-export default Comp;
