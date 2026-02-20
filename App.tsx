a715cec17a478e5e5e8e5b8c3f2f82ead47a9c82
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-02-20 18:56:30 +0530

    custom modal

--- App.tsx
+++ App.tsx
@@ -1,79 +1,44 @@
 import { useState } from 'react';
-import { View, Text,Pressable,StyleSheet,ActivityIndicator,StatusBar,Platform} from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const App=()=>{
-   const [show,setShow]=useState(false)
-   const displayIndicator=()=>{
-    setShow(true);
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <View style={style.mainStyle}>
+     {showModal==true?
+      <View style={style.mainModal}>
+        <View style={style.innerModal}>
+          <Text>ALERT TEXT</Text>
+          <Button title="Close" onPress={()=>setShowModal(false)}/>
+        </View>
+      </View>:null
 
-    setTimeout(() => {
-      setShow(false);
-    },300);
-   }
-  return(
-<View style={style.main}>
-  <StatusBar backgroundColor={'green'}
-  hidden={show}
-  />
-  <ActivityIndicator color={'black'} size={50} animating={show}>
-
-  </ActivityIndicator>
-  <Pressable onPress={()=>displayIndicator()}  >
-    <View>
-      <Text>platform: {Platform.OS}</Text>
-    {
-      Platform.OS==='android'?
-     <View >
-    <Text style={style.PressableButtonAndroid}>Press</Text>
-    </View>:
-    <View>
-     <Text style={style.PressableButtonIos}>Press</Text>
+     }
+      <Button title="Press" onPress={()=>setShowModal(true)} />
     </View>
-    }
-    </View>
-    
-  </Pressable>
-</View>
+  );
+};
 
-  )
-}
-
-const style=StyleSheet.create({
-  main:{
+const style = StyleSheet.create({
+  mainStyle: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  mainModal: {
     flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(50,50,50,.5)'
+  },
+  innerModal: {
+    backgroundColor:'white',
+    height:200,
+    width:250,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    padding:10,
+    borderRadius:5
   },
-  PressableButtonAndroid:{
-   backgroundColor:'green',
-   color:'white',
-   height:"auto",
-   width:200,
-   textAlign:'center',
-   justifyContent:'center',
-   margin:10,
-   padding:10,
-   shadowColor:'black',
-   elevation:20,
-   borderWidth:2,
-   borderColor:'green',
-   borderRadius:10
-  },
-  PressableButtonIos:{
-    backgroundColor:'red',
-   color:'white',
-   height:"auto",
-   width:200,
-   textAlign:'center',
-   justifyContent:'center',
-   margin:10,
-   padding:10,
-   shadowColor:'black',
-   elevation:25,
-   borderWidth:3,
-   borderColor:'red',
-   borderRadius:10
-  }
-})
+});
 
 export default App;
