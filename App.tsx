98d9513fb997a92513f6c68b273a0103c7cad535
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-02-22 18:09:29 +0530

    navigation styling

--- App.tsx
+++ App.tsx
@@ -7,21 +7,47 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'orange',
+          },
+          headerTitleStyle: {
+            fontSize: 20,
+          },
+          headerTintColor: 'white',
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerTitle: 'USER LOGIN',
+            headerTitleStyle: {
+              fontSize: 15,
+            },
+            headerStyle: {
+              backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+            headerTitleAlign: 'center',
+          }}
+        />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name='AboutUs' component={AboutUs}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const Login = (props) => {
+const Login = props => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 30, textAlign: 'center' }}>
         Welcome to login page login from there
       </Text>
       <Button title="home" onPress={() => props.navigation.navigate(Home)} />
+       <Button title='AboutUs' onPress={()=> props.navigation.navigate(AboutUs)}/> 
     </View>
   );
 };
@@ -31,6 +57,15 @@ const Home = () => {
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 30, textAlign: 'center' }}>
         Hello welcome to Home Screen
+      </Text>
+    </View>
+  );
+};
+const AboutUs = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30, textAlign: 'center',margin:10 }}>
+        Hello welcome to AboutUs  Screen
       </Text>
     </View>
   );
