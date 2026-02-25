4f2c945324d50e9e25b86a92f5bd4b1b7d2f7731
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-02-25 19:58:04 +0530

    pass data to screens

--- component/Home.js
+++ component/Home.js
@@ -1,10 +1,14 @@
-import { View, Text, Button } from 'react-native';
-export const Home = () => {
+
+import { View, Text} from 'react-native';
+export const Home = (props) => {
+  const {userName,userAge}=props.route.params;
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 30, textAlign: 'center' }}>
         Hello welcome to Home Screen
       </Text>
+      <Text style={{fontSize:20}}>Name: {userName}</Text>
+      <Text style={{fontSize:20}}>Age: {userAge}</Text>
     </View>
   );
 };
--- component/Login.js
+++ component/Login.js
@@ -1,12 +1,35 @@
-import { View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
 import { Home } from './Home';
 export const Login = props => {
+  const [userName, setUserName] = useState('Alice');
+  const [userAge, setUserAge] = useState(2);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 30, textAlign: 'center' }}>
         Welcome to login page login from there
       </Text>
-      <Button title="home" onPress={() => props.navigation.navigate(Home)} />
+      <TextInput
+        style={{
+          fontSize: 20,
+          borderWidth: 2,
+          borderRadius: 5,
+          borderColor: '#000',
+        }}
+        placeholder="Enter Name"
+        onChangeText={text => setUserName(text)}
+      />
+      <TextInput
+        style={{
+          fontSize: 20,
+          borderWidth: 2,
+          borderRadius: 5,
+          borderColor: '#000',
+        }}
+        placeholder="Enter Age"
+        onChangeText={text => setUserAge(text)}
+      />
+      <Button title="Home" onPress={() => props.navigation.navigate("Home",{userName,userAge})} />
     </View>
   );
 };
