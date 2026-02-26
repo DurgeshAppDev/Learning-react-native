069f947be8c10037534b5983377da578d894cd59
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-02-26 19:28:44 +0530

    fetching api

--- App.tsx
+++ App.tsx
@@ -1,19 +1,30 @@
-import { useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { Login } from './component/Login';
-import { Home } from './component/Home';
-const Tab = createBottomTabNavigator();
+import { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
 const App = () => {
+  const [data,setData]=useState<any>(null);
+  const getAPIData = async() => {
+    let url = 'https://jsonplaceholder.typicode.com/posts/1';
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result)
+  };
+  useEffect(()=>{
+   getAPIData();
+  },[])
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-      <Tab.Screen  name='Login' component={Login}/>
-      <Tab.Screen  name='Home' component={Home}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+  <View >
+      <Text style={{fontSize:20}}>Hello World</Text>
+      {
+        data?<View>
+         <Text style={{fontSize:20}}>{data.userId}</Text>
+         <Text style={{fontSize:20}}>{data.id}</Text>
+         <Text style={{fontSize:20}}>{data.title}</Text>
+         <Text style={{fontSize:20}}>{data.body}</Text>
+        </View>:
+        null
+      }
+    </View>
   );
 };
-
 export default App;
