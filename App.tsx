27a3268ea396a60f2ed0ce941e131e9c55791ed9
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-02-27 20:18:50 +0530

    large api data fecthing

--- App.tsx
+++ App.tsx
@@ -1,30 +1,38 @@
 import { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 const App = () => {
-  const [data,setData]=useState<any>(null);
-  const getAPIData = async() => {
-    let url = 'https://jsonplaceholder.typicode.com/posts/1';
-    let result = await fetch(url);
-    result = await result.json();
-    setData(result)
+  const [data, setData] = useState<any[]>([]);
+  const getAPIData = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    let response = await fetch(url);
+   let result = await response.json();
+    setData(result);
   };
-  useEffect(()=>{
-   getAPIData();
-  },[])
+  useEffect(() => {
+    getAPIData();
+  }, []);
   return (
-  <View >
-      <Text style={{fontSize:20}}>Hello World</Text>
-      {
-        data?<View>
-         <Text style={{fontSize:20}}>{data.userId}</Text>
-         <Text style={{fontSize:20}}>{data.id}</Text>
-         <Text style={{fontSize:20}}>{data.title}</Text>
-         <Text style={{fontSize:20}}>{data.body}</Text>
-        </View>:
-        null
-      }
-    </View>
+    <ScrollView>
+      <Text style={{ fontSize: 20 }}>Fetching Api Data</Text>
+      {data.length
+        ? data.map(items => (
+            <View
+              style={{
+                padding: 5,
+                borderBottomWidth: 2,
+                borderBottomColor: '#000',
+              }}
+            >
+              <Text style={{ fontSize: 15, backgroundColor: '#8f7575' }}>
+                {items.id}
+              </Text>
+              <Text style={{ fontSize: 15 }}>{items.title}</Text>
+              <Text style={{ fontSize: 15 }}>{items.body}</Text>
+            </View>
+          ))
+        : null}
+    </ScrollView>
   );
 };
 export default App;
