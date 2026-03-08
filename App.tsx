fd9011c0e8d280d4577aea85da606f25b6db7b4d
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-03-08 19:11:11 +0530

    post Api form data

--- App.tsx
+++ App.tsx
@@ -1,41 +1,61 @@
-import { useEffect, useState } from 'react';
-import { View, Text, ScrollView, FlatList } from 'react-native';
+import { useState } from 'react';
+import { View, TextInput,StyleSheet,Button } from 'react-native';
+
 
 const App = () => {
-  const [data, setData] = useState<any[]>([]);
-  const getAPIData = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    let response = await fetch(url);
-    let result = await response.json();
-    setData(result);
-  };
-  useEffect(() => {
-    getAPIData();
-  }, []);
+  const [name ,setName]=useState('');
+  const [age ,setAge]=useState(0);
+  const [email ,setemail]=useState('');
+ 
+
+  const addData= async ()=>{
+    const url="http://10.0.2.2:3000/users";
+
+    const response = await fetch(url);
+      const users = await response.json();
+  const newId = users.length + 1;
+
+    const result= await fetch(url,
+    {method :'POST',
+    headers:{'content-type':"application/json"},
+    body:JSON.stringify({newId,name,age,email})}
+    );
+  }
   return (
-    <ScrollView>
-      <Text style={{ fontSize: 22 }}>Fetching Api Data</Text>
-      {data.length ? (
-        <FlatList
-          data={data}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                padding: 5,
-                borderBottomWidth: 2,
-                borderBottomColor: '#000',
-              }}
-            >
-              <Text style={{ fontSize: 15, backgroundColor: '#8f7575' }}>
-                {item.id}
-              </Text>
-              <Text style={{ fontSize: 15 }}>{item.title}</Text>
-              <Text style={{ fontSize: 15 }}>{item.body}</Text>
-            </View>
-          )}
-        />
-      ) : null}
-    </ScrollView>
+    <View  style={Styles.main}>
+      <TextInput 
+      style={Styles.textFeild}
+       value={name} 
+       placeholder="Enter name" 
+        onChangeText={(text)=>setName(text)} />
+      <TextInput 
+      style={Styles.textFeild}
+       value={age.toString()} 
+       placeholder="Enter age" 
+        onChangeText={(text)=>setAge(text)} />
+      <TextInput 
+      style={Styles.textFeild}
+       value={email} 
+       placeholder="Enter email" 
+        onChangeText={(text)=>setemail(text)} />
+        
+        <Button  
+        title='Submit data ' 
+        onPress={()=> addData()
+        }/>
+    </View>
   );
 };
+
+const Styles=StyleSheet.create({
+  main:{
+  marginTop:20
+  },
+  textFeild:{
+    borderWidth:1,
+    borderColor:'black',
+    margin:5
+  }
+})
+
 export default App;
