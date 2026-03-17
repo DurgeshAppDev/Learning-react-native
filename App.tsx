0cc2e543aff9bffbe369ceb7fc2a95e0edad08a2
DurgeshAppDev <mr.durgeshkumar55@gmail.com>
2026-03-17 22:33:35 +0530

    search api

--- App.tsx
+++ App.tsx
@@ -1,196 +1,48 @@
-import { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  Modal,
-  ScrollView,
-  TextInput,
-} from 'react-native';
-
+import { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 const App = () => {
-  const [data, setData] = useState<any>([]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [updateModal, setUpdateModal] = useState(false);
-  const [updateData, setUpdateData] = useState(undefined);
+  const [data, setdata] = useState<any>([]);
 
-  const apiData = async () => {
-    const url = 'http://10.0.2.2:3000/users';
+  const searchAPI = async (text: any) => {
+    const url = `http://10.0.2.2:3000/users?q=${text}`;
     let result = await fetch(url);
     result = await result.json();
+    console.log(result);
     if (result) {
-      setData(result);
+      setdata(result);
     }
   };
-  const deleteUser = async (id: any) => {
-    const url = 'http://10.0.2.2:3000/users';
-    let result = await fetch(`${url}/${id}`, {
-      method: 'delete',
-    });
-
-    console.log('user deleted');
-    setModalVisible(true);
-    setTimeout(() => {
-      apiData();
-    }, 300);
-  };
-
-  useEffect(() => {
-    apiData();
-  }, []);
-
-  const updateButton = (data: any) => {
-    setUpdateModal(true);
-    setUpdateData(data);
-  };
   return (
-    <View style={styles.container}>
-      <View style={styles.apiDataStyle}>
-        <View style={{ flex: 1 }}>
-          <Text>NAME</Text>
-        </View>
-        <View style={{ flex: 1.1 }}>
-          <Text>AGE</Text>
-        </View>
-        <View style={{ flex: 1 }}>
-          <Text>OPERATION</Text>
-        </View>
-      </View>
-      <ScrollView>
-        {data.length
-          ? data.map((items: any) => (
-              <View key={items.id} style={styles.apiDataStyle}>
-                <View style={{ flex: 1.5 }}>
-                  <Text>{items.name}</Text>
-                </View>
-                <View style={{ flex: 1 }}>
-                  <Text>{items.age}</Text>
-                </View>
-                <View style={{ flex: 1 }}>
-                  <Button
-                    title="update"
-                    onPress={() => {
-                      updateButton(items);
-                    }}
-                  />
-                </View>
-                <View style={{ flex: 1 }}>
-                  <Button
-                    title="delete"
-                    onPress={() => {
-                      deleteUser(items.id);
-                    }}
-                  />
-                </View>
-              </View>
-            ))
-          : null}
-
-        <Modal visible={modalVisible} transparent={true} animationType="slide">
-          <View style={styles.modalContainer}>
-            <View style={styles.modalBox}>
-              <Text>User Deleted Successfully</Text>
-              <Button title="ok" onPress={() => setModalVisible(false)} />
+    <View>
+      <Text style={{ textAlign: 'center', fontSize: 20 }}> Search here </Text>
+      <TextInput
+        style={{ fontSize: 20, borderWidth: 1, borderColor: '#000' }}
+        placeholder="search"
+        onChangeText={text => searchAPI(text)}
+      />
+      {data.length ? (
+        data.map((item: any) => {
+          return (
+            <View>
+              <Text style={{ fontSize: 20, color: '#d66868' }}>
+                Name: {item.name}
+              </Text>
+              <Text style={{ fontSize: 20, color: '#d66868' }}>
+                Age: {item.age}
+              </Text>
             </View>
-          </View>
-        </Modal>
-
-        <Modal visible={updateModal} transparent={true} animationType="slide">
-          <UpdateModal
-            setUpdateModal={setUpdateModal}
-            updateData={updateData}
-            apiData={apiData}
-          />
-        </Modal>
-      </ScrollView>
-    </View>
-  );
-};
-
-const UpdateModal = ({ setUpdateModal, updateData, apiData }: any) => {
-  console.log(updateData);
-  const [name, setName] = useState(undefined);
-  const [age, setAge] = useState(undefined);
-
-  useEffect(() => {
-    if (updateData) {
-      setName(updateData.name);
-      setAge(updateData.age.toString());
-    }
-  }, [updateData]);
-
-  const userUpdate = async () => {
-    const url = 'http://10.0.2.2:3000/users';
-    const id = updateData.id;
-    let result = await fetch(`${url}/${id}`, {
-      method: 'Put',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({ name, age }),
-    });
-
-    result = await result.json();
-    if (result) {
-      apiData();
-      setUpdateModal(false);
-    }
-  };
-  return (
-    <View style={styles.modalContainer}>
-      <View style={styles.modalBox}>
-        <TextInput
-          style={styles.inputFeild}
-          value={name}
-          onChangeText={(text: any) => setName(text)}
-        />
-        <TextInput
-          style={styles.inputFeild}
-          value={age}
-          onChangeText={(text: any) => setAge(text)}
-        />
-        <View style={{ marginBottom: 6 }}>
-          <Button title="save" onPress={userUpdate} />
+          );
+        })
+      ) : (
+        <View>
+          <Text style={{ textAlign: 'center', marginTop: 10 }}>
+            No results found
+          </Text>
         </View>
-        <Button title="close" onPress={() => setUpdateModal(false)} />
-      </View>
+      )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  apiDataStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-    backgroundColor: 'green',
-    margin: 3,
-    borderRadius: 5,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  modalBox: {
-    backgroundColor: 'green',
-    alignItems: 'center',
-    padding: 20,
-    borderRadius: 5,
-    margin: 5,
-  },
-  inputFeild: {
-    borderWidth: 2,
-    width: 300,
-    backgroundColor: '#fff',
-    marginBottom: 5,
-  },
-});
 
 export default App;
